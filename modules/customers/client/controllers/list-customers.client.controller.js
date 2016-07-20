9a5f1cd51adcc97e83a286975e8a87d6dfd10de7
Elvis Sun <elvissunlihe@gmail.com>
2016-07-20 16:56:29 -0400

    Update job list view

---
+++ modules/customers/client/controllers/list-customers.client.controller.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('customers')
+    .controller('CustomersListController', CustomersListController);
+
+  CustomersListController.$inject = ['CustomersService'];
+
+  function CustomersListController(CustomersService) {
+    var vm = this;
+
+    vm.customers = CustomersService.query();
+  }
+})();
---
+++ modules/customers/client/services/customers.client.service.js
@@ -0,0 +1,20 @@
+//Customers service used to communicate Customers REST endpoints
+(function () {
+  'use strict';
+
+  angular
+    .module('customers')
+    .factory('CustomersService', CustomersService);
+
+  CustomersService.$inject = ['$resource'];
+
+  function CustomersService($resource) {
+    return $resource('api/customers/:customerId', {
+      customerId: '@_id'
+    }, {
+      update: {
+        method: 'PUT'
+      }
+    });
+  }
+})();
