50dbaa76247b0255a0141a4b0767bd2a565119a3
Elvis Sun <elvissunlihe@gmail.com>
2016-07-22 16:28:22 -0400

    update client side view

--- modules/jobs/client/controllers/jobs.client.controller.js
+++ modules/jobs/client/controllers/jobs.client.controller.js
@@ -52,73 +52,61 @@
 
 
 
-  $scope.today = function() {
-    $scope.dt = new Date();
-  };
-  $scope.today();
+    $scope.today = function() {
+      $scope.dt = new Date();
+    };
+    $scope.today();
 
-  $scope.clear = function () {
-    $scope.dt = null;
-  };
+    $scope.clear = function () {
+      $scope.dt = null;
+    };
 
-  // Disable weekend selection
-  $scope.disabled = function(date, mode) {
-    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-  };
+    $scope.open = function($event) {
+      $scope.status.opened = true;
+    };
 
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
-  $scope.toggleMin();
-  $scope.maxDate = new Date(2020, 5, 22);
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
 
-  $scope.open = function($event) {
-    $scope.status.opened = true;
-  };
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = 'dd-MMMM-yyyy';
 
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    startingDay: 1
-  };
+    $scope.status = {
+      opened: false
+    };
 
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
+    var tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var afterTomorrow = new Date();
+    afterTomorrow.setDate(tomorrow.getDate() + 2);
+    $scope.events =
+      [
+        {
+          date: tomorrow,
+          status: 'full'
+        },
+        {
+          date: afterTomorrow,
+          status: 'partially'
+        }
+      ];
 
-  $scope.status = {
-    opened: false
-  };
+    $scope.getDayClass = function(date, mode) {
+      if (mode === 'day') {
+        var dayToCheck = new Date(date).setHours(0,0,0,0);
 
-  var tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  var afterTomorrow = new Date();
-  afterTomorrow.setDate(tomorrow.getDate() + 2);
-  $scope.events =
-    [
-      {
-        date: tomorrow,
-        status: 'full'
-      },
-      {
-        date: afterTomorrow,
-        status: 'partially'
-      }
-    ];
+        for (var i=0;i<$scope.events.length;i++){
+          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
 
-  $scope.getDayClass = function(date, mode) {
-    if (mode === 'day') {
-      var dayToCheck = new Date(date).setHours(0,0,0,0);
-
-      for (var i=0;i<$scope.events.length;i++){
-        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-
-        if (dayToCheck === currentDay) {
-          return $scope.events[i].status;
+          if (dayToCheck === currentDay) {
+            return $scope.events[i].status;
+          }
         }
       }
-    }
-
-    return '';
-  };
+      return '';
+    };
 
 
 
