d2c01208f2e1b504faf36645506919ddad29efa5
Elvis Sun <elvissunlihe@gmail.com>
2016-07-22 15:01:10 -0400

    update view list

--- config/env/development.js
+++ config/env/development.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://yourtech:databasepassword@ds027165.mlab.com:27165' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/yourtech-dev',
     options: {
-      user: '',
-      pass: ''
+      user: 'yourtech',
+      pass: 'databasepassword'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
